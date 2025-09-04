36f2947708bb012bc3dc9e08eaf7434fc51e6224
lolitosuarews2dfhnky <lolitosuare9s@gmail.com>
2025-09-03 19:36:12 -0500

    Agregados ejercicios 1 a 5

---
+++ ejercicio5.py
@@ -0,0 +1,4 @@
+horas = float(input("Ingresa el número de horas trabajadas: "))
+coste = float(input("Ingresa el coste por hora: "))
+paga = horas * coste
+print("La paga que le corresponde es:", paga)
